9af663b7cce24fd9c9f927d9bb941a701f977f15
David Ching <dching@demandware.com>
2014-08-06 23:45:00 -0400

    Initial check in

---
+++ PyHello2/src/goodbye.py
@@ -0,0 +1,11 @@
+'''
+Created on Aug 6, 2014
+
+@author: dching
+'''
+
+a = 1
+
+
+if __name__ == '__main__':
+    print "goodbye %d" % a
---
+++ PyHello2/src/hello2.py
@@ -0,0 +1,11 @@
+'''
+Created on Aug 6, 2014
+
+@author: dching
+'''
+x = 1
+y = 2
+
+
+if __name__ == '__main__':
+    print "hello world %d %d" % (x, y)
